fc2e01776060b6a91119280ad430238ed6094ea9
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-01-13 19:15:20 -0600

    delete timedelta import

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from datetime import timedelta
 
 from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
