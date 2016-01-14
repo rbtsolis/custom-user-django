55924b074f5905ce90e293aec2cab62df6ade7f5
Roberth Solís Martínez <roberth.solis@gmail.com>
2016-01-13 21:22:40 -0600

    Login, logout functions of django

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import logout, authenticate, login
+from django.http import JsonResponse, HttpResponse, Http404
 
-# Create your views here.
+
+def Logout(request):
+    logout(request)
+    return redirect('/')
+
+
+def loginAjax(request):
+
+    if request.is_ajax():
+
+        username_ajax = request.POST['username']
+        password_ajax = request.POST['password']
+
+        user = authenticate(username = username_ajax,
+            password = password_ajax)
+        if user is not None and user.is_active:
+            login(request, user)
+            response = JsonResponse({'status_login': 'success'})
+        else:
+            response = JsonResponse({'status_login': 'error'})
+        return HttpResponse(response.content)
+    else:
+        raise Http404
